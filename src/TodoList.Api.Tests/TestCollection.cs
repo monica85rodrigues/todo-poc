c7648d8523b8a65ae24fc07a432b31d907b0c218
Mónica Rodrigues <monica.rodrigues@farfetch.com>
2020-11-21 19:10:30 +0000

    Add integration test for GET all todos

---
+++ src/TodoList.Api.Tests/TestCollection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace TodoList.Api.Tests
+{
+    [CollectionDefinition("Integration Tests", DisableParallelization = true)]
+    public class TestCollection : ICollectionFixture<WebApplicationFactory<TodoList.Startup.Startup>>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
---
+++ src/TodoList.Api.Tests/TodosApiControllerTests.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TodoList.Contracts.Dtos;
+using Xunit;
+
+namespace TodoList.Api.Tests
+{
+    [Collection("Integration Tests")]
+    public class TodosApiControllerTests
+    {
+        private readonly WebApplicationFactory<Startup.Startup> _factory;
+
+         public TodosApiControllerTests(WebApplicationFactory<Startup.Startup> factory)
+         {
+             _factory = factory;
+         }
+        
+        [Fact]
+        public async Task GetTodos_ReturnsSuccess()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            
+            // Act
+            var response = await client.GetAsync("api/todos");
+            var todos = JsonSerializer.Deserialize<IEnumerable<Todo>>(
+                await response.Content.ReadAsStringAsync(),
+                new JsonSerializerOptions {PropertyNameCaseInsensitive = true});
+            
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.NotNull(response.Content);
+            Assert.NotNull(todos);
+            Assert.Equal(3, todos.Count());
+        }
+    }
+}
